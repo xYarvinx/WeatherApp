6442e2cf12abffb1a8e8c2c1ee04b4aacc8f5182
Yaroslav <otebator@gmail.com>
2024-01-07 15:22:56 +1000

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ярослав on 07.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
